9ed75b88a75f79823555d925fc3c37a6d9b20d10
RaviRanjanKr <82093616+raviranjankr@users.noreply.github.com>
2021-12-03 15:33:48 +0530

    Revert "Update Program.cs"
    This reverts commit 837754a957bb26e88beedf5e59569cfcf8df0fad.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,7 +12,6 @@ namespace HelloWorld
             Console.WriteLine($"{msz1} {msz2} {msz3}");
             msz1 = "Hi";
             Console.WriteLine($"{msz1} {msz2} {msz3}");
-            // Testing comments 
             Console.ReadLine();
         }
     }
