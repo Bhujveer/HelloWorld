7e8379effa47c2c903d8d400cca028d0b57327e1
RaviRanjanKr <82093616+raviranjankr@users.noreply.github.com>
2021-12-03 15:05:52 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msz1 = "Hello World!";
+            var msz2 = "I am using";
+            var msz3 = "GITHUB";
+            Console.WriteLine($"{msz1} {msz2} {msz3}");
+            Console.ReadLine();
+        }
+    }
+}
